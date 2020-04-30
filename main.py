aa69a9b99015032d59da6c7cf81a7667e4e73b32
Tomás Antunes <tomasantunes@gmail.com>
2020-04-30 15:48:48 +0100

    Update

---
+++ main.py
@@ -0,0 +1,35 @@
+from pandas import DataFrame, read_csv
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request, flash, jsonify, session, redirect, Markup
+import sqlite3
+import datetime
+import os
+import random
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+file = r'articles.csv'
+df = pd.read_csv(file)
+
+df1 = df.sort_values(by="claps", ascending=False).head(20)
+
+print(df1)
+
+def getRandomQuote():
+	text = df['text'].unique().tolist()
+	text = "\n".join(text)
+	n_chars = len(text)
+	r = random.randint(1, n_chars)
+	return text[r:r + 250].split(' ', 1)[1]
+
+@app.route("/")
+def home():
+	top20 = df1['title'].unique().tolist()
+	quote = getRandomQuote()
+	return render_template("home.html", top20=top20, quote=quote)
+
+if __name__ == "__main__":
+	app.run()
